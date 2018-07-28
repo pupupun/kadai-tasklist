bfe06aae70c46270678e487b83fc6ea9174434aa
shio.chika <shipoo2222@gmail.com>
2018-07-28 13:45:05 +0000

    user login tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
 
   root to: "toppages#index"
+  
+  get 'toppages/index'
   
   get 'sessions/new'
 
@@ -15,6 +23,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-
-  resources :tasks, only: [:index, :show, :new, :create]
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end
