915c8ceb723d30dc718533525bec4681fb9dadda
shio.chika <shipoo2222@gmail.com>
2018-07-28 11:56:51 +0000

    toppage tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  root to: "toppages#index"
+  
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
-  resources :tasks
+  resources :tasks, only: [:index, :show, :new, :create]
 end
